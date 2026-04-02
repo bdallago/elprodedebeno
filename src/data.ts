f48b925e22f568284178bea48312d92dd33e97f6
Benito Dal Lago <102180144+bdallago@users.noreply.github.com>
2026-04-02 14:39:06 -0300

    Correct spelling of 'Algeria' to 'Argelia'

--- src/data.ts
+++ src/data.ts
@@ -8,7 +8,7 @@ export const GROUPS = {
   G: ["Bélgica", "Egipto", "Irán", "Nueva Zelanda"],
   H: ["España", "Cabo Verde", "Arabia Saudita", "Uruguay"],
   I: ["Francia", "Senegal", "Irak", "Noruega"],
-  J: ["Argentina", "Algeria", "Austria", "Jordania"],
+  J: ["Argentina", "Argelia", "Austria", "Jordania"],
   K: ["Portugal", "República Democrática del Congo", "Uzbekistán", "Colombia"],
   L: ["Inglaterra", "Croacia", "Ghana", "Panamá"]
 };
